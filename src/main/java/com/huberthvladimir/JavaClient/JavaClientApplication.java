5e45469dac74a3185e6c5e3448bfcaff009088ac
Huberth Vladimir <vladimirhuberth@hotmail.com>
2024-12-29 19:02:38 -0300

    first commit

---
+++ src/main/java/com/huberthvladimir/JavaClient/JavaClientApplication.java
@@ -0,0 +1,13 @@
+package com.huberthvladimir.JavaClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaClientApplication.class, args);
+	}
+
+}
